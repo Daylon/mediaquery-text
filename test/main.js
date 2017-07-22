c85e052555fea54e9b675a1ccbd22a7ba1407d41
Julien Bouvet <daylonmw@outlook.com>
2017-07-22 09:47:27 +0200

    Adding failing test as per issue #1

--- test/main.js
+++ test/main.js
@@ -14,3 +14,11 @@ describe('mediaquery-text', function() {
         done();
     });
 });
+
+describe('mediaquery-text', function() {
+    it('Should return Media Query containing a font-face rule', function(done) {
+        var mediaQueries = mediaQueryText(String(fs.readFileSync(path.join('test', 'fixtures', 'font-face.css'))));
+        mediaQueries.should.be.equal(String(fs.readFileSync(path.join('test', 'expected', 'font-face.css'))));
+        done();
+    });
+});
